91f599e109a3d271848818a42ec041620c180306
raikko800 <roberttangerman@gmail.com>
2014-02-06 18:37:45 +0100

    initial commit

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	private GameObject player;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update () {
+		Vector3 playerPosition 	= player.transform.position;
+		this.transform.position = new Vector3(playerPosition.x, playerPosition.y, this.transform.position.z);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float velocity;
+
+	private Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.velocity = new Vector2(velocity, 0f);
+	}
+}
